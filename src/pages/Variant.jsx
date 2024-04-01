d2d763e03266df995ff20a93ce501a0ab40936d5
winniekagz <winniekagendo35@gmail.com>
2024-04-01 17:38:08 +0300

    sd

--- src/pages/Variant.jsx
+++ src/pages/Variant.jsx
@@ -79,8 +79,7 @@ function Variant() {
     }
       generateCombinations(optionList, 0, "");
       setShowForm(false);
-      handleRemoveParentFinish(index);
-         generateCombinations(optionList, 0, "");
+      handleRemoveParentFinish(index); 
       window.location.reload();
   };
 
@@ -102,12 +101,12 @@ function Variant() {
 
 
   const [combinations, setCombinations] = useState([]);
-  function generateCombinations(data, index, combination) { 
-     if (index === data?.length) {
+  function generateCombinations(data, index, combination) {  
+     if (index === data?.length) { 
        setCombinations((prevCombinations) => { 
-         if (!prevCombinations.includes(combination.trim())) {
+         if (!prevCombinations.includes(combination.trim())) { 
            return [...prevCombinations, combination.trim()];
-         } else {
+         } else { 
            return prevCombinations;  
          }
        });
@@ -137,7 +136,7 @@ function Variant() {
     generateCombinations(optionList, 0, "");
   }, [optionList]);
 
-
+ 
   return (
     <div className="bg-light-blue-bg min-h-[100vh]">
       <div className="flex flex-col justify-center py-12 items-center ">
